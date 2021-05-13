bb83191aa5527c535b5cf471269008852f63d279
ahsouza <annibalhsouza@gmail.com>
2021-05-12 22:52:49 -0300

    feat: implementar busca de endereço por cep

--- client/src/template/checkout/AddressForm.tsx
+++ client/src/template/checkout/AddressForm.tsx
@@ -1,8 +1,28 @@
 import * as React from 'react'
 import Grid from '@material-ui/core/Grid'
 import TextField from '@material-ui/core/TextField'
+import axios from 'axios'
 
 export default function AddressForm() {
+  const [cep, setCep] = React.useState(0)
+  const [bairro, setBairro] = React.useState('')
+  const [logradouro, setLogradouro] = React.useState('')
+  const [cidade, setCidade] = React.useState('')
+  const [estado, setEstado] = React.useState('')
+
+  const handleChangeCep = (event: React.ChangeEvent<{ value: unknown }>) => {
+    setCep(event.target.value as number)
+  }
+  const handleBlur = (event: React.ChangeEvent<{ value: unknown }>) => {
+    axios.get(`https://viacep.com.br/ws/${cep}/json`, {})
+    .then(res => {
+      setCidade(res.data.localidade)
+      setBairro(res.data.bairro)
+      setLogradouro(res.data.logradouro)
+      setEstado(res.data.uf)
+    })
+  }
+
   return (
     <React.Fragment>
       <br/>
@@ -10,12 +30,15 @@ export default function AddressForm() {
         <Grid item xs={3} sm={4}>
           <TextField
             required
+            onChange={handleChangeCep}
+            onBlur={handleBlur}
             id="cep"
             name="cep"
             label="Cep"
             fullWidth
-            autoComplete="2916...."
+            autoComplete="2021...."
             variant="standard"
+            inputProps={{ maxLength: 8 }}
           />
         </Grid>
         <Grid item xs={6} sm={4}>
@@ -23,6 +46,7 @@ export default function AddressForm() {
             id="bairro"
             name="bairro"
             label="Bairro"
+            value={bairro}
             fullWidth
             variant="standard"
           />
@@ -37,13 +61,23 @@ export default function AddressForm() {
             variant="standard"
           />
         </Grid>
-        <Grid item xs={12} sm={6}>
+        <Grid item xs={10} sm={8}>
           <TextField
             id="cidade"
             name="cidade"
             label="Cidade"
+            value={cidade}
             fullWidth
-            autoComplete="Salvador..."
+            variant="standard"
+          />
+        </Grid>
+        <Grid item xs={2} sm={2}>
+          <TextField
+            id="estado"
+            name="estado"
+            label="Estado"
+            value={estado}
+            fullWidth
             variant="standard"
           />
         </Grid>
@@ -53,7 +87,7 @@ export default function AddressForm() {
             name="logradouro"
             label="Logradouro"
             fullWidth
-            autoComplete="Rua das orqúideas..."
+            value={logradouro}
             variant="standard"
           />
         </Grid>
